94b2d2c97cc94706a7743a3e0bcb856a225175a1
Luis <luisfernandosantaospina@gmail.com>
2022-11-03 16:26:07 -0500

    Corrige componente de book

--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookListComponent } from './book-list/book-list.component';
 import { BookDetailComponent } from './book-detail/book-detail.component';
-import {}
 
 @NgModule({
   declarations: [
